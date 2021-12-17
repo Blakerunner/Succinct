9f54889d65ca2318bcbd87ed44c342e8bc4354e5
Shagun <54307245+Ishika18@users.noreply.github.com>
2021-12-17 14:48:54 -0800

    resources refactor

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from summarizer import get_text_from_str, get_text_from_file, get_text_from_url
+from resources.summarizer import get_text_from_url
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    get_text_from_url("https://hi.wikipedia.org/wiki/%E0%A4%AE%E0%A5%81%E0%A4%96%E0%A4%AA%E0%A5%83%E0%A4%B7%E0%A5%8D%E0%A4%A0")
     return "Hello World!!"
 
 
