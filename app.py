5606dbb09a476e234052ecb6f8bacc73e44ca289
Shagun <54307245+Ishika18@users.noreply.github.com>
2021-12-17 13:34:36 -0800

    summarizer bug fix

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from parser import get_text_from_str, get_text_from_file, get_text_from_url
+from summarizer import get_text_from_str, get_text_from_file, get_text_from_url
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return "Hello World!!"
 
 
 if __name__ == '__main__':
--- summarizer.py
+++ summarizer.py
@@ -7,34 +7,32 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
-from googletrans import Translator
-
-from language_types import LanguageTypes
+import urllib.request
 
 SENTENCES_COUNT = 10
 LANGUAGE = "english"
 
 
-def get_text_from_url(url: str, language: str) -> list:
+def get_text_from_url(url: str) -> list:
     """
     Return summarized text from the url.
 
     :param url: a string
     :return: list of strings
     """
-    parser = HtmlParser.from_url(url, Tokenizer(language))
-    return get_summary_from_parser(parser)
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    return get_summary_from_parser(parser, LANGUAGE)
 
 
-def get_text_from_file(file_name: str, language: str) -> list:
+def get_text_from_file(file_name: str) -> list:
     """
     Return summarized text from the text file.
 
     :param file_name: a str, language: string
     :return: list of strings
     """
-    parser = PlaintextParser.from_file(file_name, Tokenizer(language))
-    return get_summary_from_parser(parser)
+    parser = PlaintextParser.from_file(file_name, Tokenizer(LANGUAGE))
+    return get_summary_from_parser(parser, LANGUAGE)
 
 
 def get_text_from_word():
@@ -49,15 +47,15 @@ def get_text_from_txt():
     pass
 
 
-def get_text_from_str(text: str, language: str) -> list:
+def get_text_from_str(text: str) -> list:
     """
     Return summarized text from the text string.
 
     :param text: a string
     :return: list of strings
     """
-    parser = PlaintextParser.from_string(text, Tokenizer(languageE))
-    return get_summary_from_parser(parser)
+    parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+    return get_summary_from_parser(parser, LANGUAGE)
 
 
 def get_summary_from_parser(parser: PlaintextParser, language: str):
