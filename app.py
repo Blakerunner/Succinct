8c022ef0d25335d0f2be867df4c76a976ca74bb5
Blake Michalzik <bmichalzik@gmail.com>
2021-12-17 14:39:10 -0800

    endpoint update

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from summarizer import get_text_from_str, get_text_from_file, get_text_from_url
 
 app = Flask(__name__)
 
+@app.route('/api/v1/file')
+def succinct_file():
+    return {'summary': "Summary of FILE text."}
+
+@app.route('/api/v1/url')
+def succinct_url():
+    return {'summary': "Summary of URL text."}
+
+@app.route('/api/v1/text')
+def succinct_text():
+    return {'summary': "Summary of TEXT text."}
 
 @app.route('/')
 def hello_world():
