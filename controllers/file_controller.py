2373adb1baf60c09d444e305db4ba7da009177b3
Blake Michalzik <bmichalzik@gmail.com>
2021-12-17 19:37:33 -0800

    update fire controller

--- controllers/file_controller.py
+++ controllers/file_controller.py
@@ -1,13 +1,15 @@
 from flask_restful import Resource, reqparse, abort
+import werkzeug
 
 class FileController(Resource):
     def get(self):
         return "This is: /api/v1/file"
 
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('file', type=str, help='Text to be summarized', required=True)
+        parse = reqparse.RequestParser()
+        parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files', help='Text to be summarized', required=True)
         args = parser.parse_args()
+        text_file = args['file']
         if args['text']:
             text = args['text']
             summary_text = self.text_summary(text)
