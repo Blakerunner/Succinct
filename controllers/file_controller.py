6e9aa2bc8d858aebdc2c2183783cb342ef3e274a
Lam Long Vu <longvulam91@gmail.com>
2021-12-17 22:14:06 -0800

    Fix file upload folder.
    Create dir if not exists.

--- controllers/file_controller.py
+++ controllers/file_controller.py
@@ -3,6 +3,7 @@ import werkzeug
 from flask import request
 from flask_restful import Resource, reqparse, abort
 from resources.summarizer import get_text_from_file
+from os.path import exists
 TEMP_FILES = './files/'
 
 class FileController(Resource):
@@ -10,9 +11,13 @@ class FileController(Resource):
         return "This is: /api/v1/file"
 
     def post(self):
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        temp_path = os.path.join(basedir, TEMP_FILES)
+        if not exists(temp_path):
+            os.mkdir(temp_path)
         if request.files:
             text_file = request.files['file']
-            text_file_path = os.path.join(TEMP_FILES, text_file.filename)
+            text_file_path = os.path.join(temp_path, text_file.filename)
             text_file.save(text_file_path)
             summary_text = get_text_from_file(text_file_path)
             os.unlink(text_file_path)
