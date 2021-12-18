f4972e948d52e1d5d643a933e503c0c35587642e
owenarando <66458556+owenarando@users.noreply.github.com>
2021-12-18 12:33:43 -0800

    Added summary length parameter with default parameters to summarization methods in summarizer.py To prepare for requests with summarization length.

--- resources/summarizer.py
+++ resources/summarizer.py
@@ -19,10 +19,10 @@ SENTENCES_COUNT = 10
 LANGUAGE = "english"
 
 
-def get_text_from_url(url: str) -> list:
+def get_text_from_url(url: str, summary_length: int = 10) -> list:
     """
     Return summarized text from the url.
-
+    :param summary_length: length of the summary in lines.
     :param url: a string
     :return: list of strings
     """
@@ -30,61 +30,65 @@ def get_text_from_url(url: str) -> list:
     language = request.headers["Content-language"]
     try:
         parser = HtmlParser.from_url(url, Tokenizer(language))
-        return get_summary_from_parser(parser, language)
+        return get_summary_from_parser(parser, language, summary_length)
     except LookupError:
         print("Language not supported")
 
 
-def get_text_from_file(file_name: str) -> list:
+def get_text_from_file(file_name: str, summary_length: int = 10) -> list:
     """
     Return summarized text from the text file.
-
+    :param summary_length: length of the summary in lines.
     :param file_name: a str, language: string
     :return: list of strings
     """
     if file_name[file_name.rfind('.') + 1:] == "pdf":
-        return get_text_from_pdf(file_name)
+        return get_text_from_pdf(file_name, summary_length)
     elif file_name[file_name.rfind('.') + 1:] == "docx":
-        return get_text_from_word(file_name)
+        return get_text_from_word(file_name, summary_length)
     else:
-        return get_text_from_txt(file_name)
+        return get_text_from_txt(file_name, summary_length)
 
 
-def get_text_from_txt(file_name:str) -> list:
+def get_text_from_txt(file_name: str, summary_length: int = 10) -> list:
     """
     Return summarized version from a text file.
+    :param summary_length: length of the summary in lines.
     :param file_name: str
     :return: list of strings
     """
     with open(file_name, "r", encoding="utf-8") as f:
         data = f.read()
-        return get_text_from_str(data)
+        return get_text_from_str(data, summary_length)
 
 
-def get_text_from_word(file_name: str) -> list:
+def get_text_from_word(file_name: str, summary_length: int = 10) -> list:
     """
     Return summarized version from a docx file.
+    :param summary_length: length of the summary in lines.
     :param file_name: str
     :return: list of strings
     """
     doc = docx2txt.process(file_name)
-    return get_text_from_str(doc)
+    return get_text_from_str(doc, summary_length)
 
 
-def get_text_from_pdf(file_name: str) -> list:
+def get_text_from_pdf(file_name: str, summary_length: int = 10) -> list:
     """
     Return summarized version from a pdf file.
+    :param summary_length: length of the summary in lines.
     :param file_name: str
     :return: list of strings
     """
     pdf = PdfFileReader(file_name)
     pdf_str = "\n".join([pdf.getPage(page).extractText() for page in range(pdf.numPages)])
-    return get_text_from_str(pdf_str)
+    return get_text_from_str(pdf_str, summary_length)
 
 
-def get_text_from_str(text: str) -> list:
+def get_text_from_str(text: str, summary_length: int = 10) -> list:
     """
     Return summarized text from the text string.
+    :param summary_length: length of the summary in lines.
     :param text: a string
     :return: list of strings
     """
@@ -93,14 +97,15 @@ def get_text_from_str(text: str) -> list:
         language = 'zh'
     try:
         parser = PlaintextParser.from_string(text, Tokenizer(language))
-        return get_summary_from_parser(parser, language)
+        return get_summary_from_parser(parser, language, summary_length)
     except LookupError:
         print("Language not supported")
 
 
-def get_summary_from_parser(parser: PlaintextParser, language: str):
+def get_summary_from_parser(parser: PlaintextParser, language: str, summary_length: int = 10):
     """
     Return summary from parsed response.
+    :param summary_length: length of the summary in lines.
     :param parser: a PlainTextParser object
     :param language: a string
     :return: a list of strings
@@ -109,6 +114,4 @@ def get_summary_from_parser(parser: PlaintextParser, language: str):
 
     summarizer = Summarizer(stemmer)
     summarizer.stop_words = get_stop_words(language)
-    return [str(sentence) for sentence in summarizer(parser.document, SENTENCES_COUNT)]
-
-
+    return [str(sentence) for sentence in summarizer(parser.document, summary_length)]
