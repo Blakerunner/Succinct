ca07ab923c702a7e02da78dfc2b41bcba8a74afe
Blake Michalzik <bmichalzik@gmail.com>
2021-12-17 17:53:19 -0800

    update text_controller

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from controllers.url_controller import UrlController
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(FileController, '/api/v1/text')
-api.add_resource(TextController, '/api/v1/file')
+api.add_resource(TextController, '/api/v1/text')
+api.add_resource(FileController, '/api/v1/file')
 api.add_resource(UrlController, '/api/v1/url')
 
 @app.route('/')
